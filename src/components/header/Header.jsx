cd835482ce58de137fe0b4e012422a0795756637
Salmon <salmonlearner34@gmail.com>
2024-06-03 00:47:39 +0500

    navbar added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,28 +1,53 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Header.scss";
-import { FaShoppingBag } from "react-icons/fa";
+import { FaShoppingBag, FaBars, FaTimes, FaSearch } from "react-icons/fa";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="header">
       <nav className="nav container">
         <div className="nav-logo">
           <Link to={"/"}>Logo</Link>
         </div>
-        <div className="nav-link">
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/service">Service</Link>
-          <Link to="/shop">Shop</Link>
-          <Link to="/contact">Contact</Link>
-          <Link to="/news">New</Link>
+        <div className={`nav-link ${menuOpen ? "open" : ""}`}>
+          <Link to="/" onClick={handleMenuToggle}>
+            Home
+          </Link>
+          <Link to="/about" onClick={handleMenuToggle}>
+            About
+          </Link>
+          <Link to="/service" onClick={handleMenuToggle}>
+            Service
+          </Link>
+          <Link to="/shop" onClick={handleMenuToggle}>
+            Shop
+          </Link>
+          <Link to="/contact" onClick={handleMenuToggle}>
+            Contact
+          </Link>
+          <Link to="/news" onClick={handleMenuToggle}>
+            New
+          </Link>
         </div>
         <div className="nav-btn">
-          <input type="text" placeholder="Search..." />
+          <div className="search-container">
+            <input type="text" placeholder="Search..." />
+            <FaSearch className="search-icon" />
+          </div>
           <button className="icon-shop">
             <FaShoppingBag />
           </button>
         </div>
+        <button className="hamburger" onClick={handleMenuToggle}>
+          {menuOpen ? <FaTimes /> : <FaBars />}
+        </button>
       </nav>
     </header>
   );
