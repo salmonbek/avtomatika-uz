c27159aee5d495f2421475fd3041503bb9dbb34a
Salmon <salmonlearner34@gmail.com>
2024-06-06 03:02:30 +0500

    hero-bg added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
-
+// import Pagination1 from "../assets/images/hero-pagination1.jpg";
+// import Pagination2 from "../assets/images/hero-pagination2.jpg";
+// import Pagination3 from "../assets/images/hero-pagination3.jpg";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <main className="main">
+        <section className="hero-section">
+          <div className="container">
+            <div className="hero-flex">
+              <div className="hero-left">
+                <h1> Industrial engineering company</h1>
+                <p>
+                  You can find the complete automation solutions and equipment
+                  you need here
+                </p>
+                <div className="hero-btn">
+                  <button className="hero-btn-1">
+                    campany license download
+                  </button>
+                  <button className="hero-btn-2">
+                    Download campany presentation
+                  </button>
+                </div>
+              </div>
+              <div className="hero-right">
+                {/* <img src={Pagination1} alt="Pagination1-image" /> */}
+                {/* <img src={Pagination2} alt="Pagination2-image" />
+                <img src={Pagination3} alt="Pagination3-image" /> */}
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque dolore
+          sint, quam numquam aut odit asperiores sequi consectetur molestias
+          assumenda sunt at, rem doloribus nulla iure provident officia facere
+          sed tenetur consequuntur. Nihil dolor rem a! Quod, ab minus velit,
+          fuga corporis enim optio doloremque sed et sit ipsum explicabo sunt
+          cupiditate aliquam ullam a asperiores eius, necessitatibus corrupti.
+          Dolor at obcaecati non laborum sed id repellendus ducimus esse vitae
+          nulla sunt magni voluptate, quidem eveniet totam iste deleniti
+          perspiciatis neque labore. Quasi ex, dolorum inventore fugiat
+          distinctio mollitia minima natus saepe pariatur perspiciatis veniam
+          atque doloremque voluptate nam praesentium
+        </section>
+      </main>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
