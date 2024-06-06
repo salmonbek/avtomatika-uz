70706301b26bcee57f4744abe826eb4c727dac41
Salmon <salmonlearner34@gmail.com>
2024-06-06 18:02:26 +0500

    hero-bg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,32 @@
-// import Pagination1 from "../assets/images/hero-pagination1.jpg";
-// import Pagination2 from "../assets/images/hero-pagination2.jpg";
-// import Pagination3 from "../assets/images/hero-pagination3.jpg";
+import { useState, useEffect } from "react";
+import Pagination1 from "../assets/images/hero-pagination1.jpg";
+import Pagination2 from "../assets/images/hero-pagination2.jpg";
+import Pagination3 from "../assets/images/hero-pagination3.jpg";
+import Pagination4 from "../assets/images/hero-pagination4.jpg"; // New image
+
+const images = [Pagination1, Pagination2, Pagination3, Pagination4]; // Include the new image
+
 const Home = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [direction, setDirection] = useState("next");
+
+  const goToNextImage = () => {
+    setDirection("next");
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const goToPrevImage = () => {
+    setDirection("prev");
+    setCurrentImageIndex(
+      (prevIndex) => (prevIndex - 1 + images.length) % images.length
+    );
+  };
+
+  useEffect(() => {
+    const interval = setInterval(goToNextImage, 3000);
+    return () => clearInterval(interval); // Cleanup interval on component unmount
+  }, []);
+
   return (
     <div>
       <main className="main">
@@ -9,40 +34,76 @@ const Home = () => {
           <div className="container">
             <div className="hero-flex">
               <div className="hero-left">
-                <h1> Industrial engineering company</h1>
+                <h1>Industrial engineering company</h1>
                 <p>
                   You can find the complete automation solutions and equipment
                   you need here
                 </p>
                 <div className="hero-btn">
                   <button className="hero-btn-1">
-                    campany license download
+                    company license download
                   </button>
                   <button className="hero-btn-2">
-                    Download campany presentation
+                    Download company presentation
                   </button>
                 </div>
               </div>
               <div className="hero-right">
-                {/* <img src={Pagination1} alt="Pagination1-image" /> */}
-                {/* <img src={Pagination2} alt="Pagination2-image" />
-                <img src={Pagination3} alt="Pagination3-image" /> */}
+                <button className="carousel-arrow left" onClick={goToPrevImage}>
+                  &#10094;
+                </button>
+                <button
+                  className="carousel-arrow right"
+                  onClick={goToNextImage}
+                >
+                  &#10095;
+                </button>
+                {images.map((image, index) => (
+                  <img
+                    key={index}
+                    src={image}
+                    alt={`Pagination${index + 1}-image`}
+                    className={
+                      index === currentImageIndex
+                        ? "active"
+                        : index ===
+                            (currentImageIndex - 1 + images.length) %
+                              images.length && direction === "next"
+                        ? "prev"
+                        : index === (currentImageIndex + 1) % images.length &&
+                          direction === "prev"
+                        ? "next"
+                        : ""
+                    }
+                  />
+                ))}
+                <div className="pagination">
+                  {images.map((_, index) => (
+                    <div
+                      key={index}
+                      className={`pagination-circle ${
+                        index === currentImageIndex ? "active" : ""
+                      }`}
+                    />
+                  ))}
+                </div>
               </div>
             </div>
           </div>
         </section>
-        <section>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque dolore
-          sint, quam numquam aut odit asperiores sequi consectetur molestias
-          assumenda sunt at, rem doloribus nulla iure provident officia facere
-          sed tenetur consequuntur. Nihil dolor rem a! Quod, ab minus velit,
-          fuga corporis enim optio doloremque sed et sit ipsum explicabo sunt
-          cupiditate aliquam ullam a asperiores eius, necessitatibus corrupti.
-          Dolor at obcaecati non laborum sed id repellendus ducimus esse vitae
-          nulla sunt magni voluptate, quidem eveniet totam iste deleniti
-          perspiciatis neque labore. Quasi ex, dolorum inventore fugiat
-          distinctio mollitia minima natus saepe pariatur perspiciatis veniam
-          atque doloremque voluptate nam praesentium
+        <section className="section">
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+            ad, commodi autem esse, eos in quisquam tempore libero natus alias
+            quo dolorem consequuntur beatae quaerat harum enim laboriosam veniam
+            impedit.
+          </div>
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+            ad, commodi autem esse, eos in quisquam tempore libero natus alias
+            quo dolorem consequuntur beatae quaerat harum enim laboriosam veniam
+            impedit.
+          </div>
         </section>
       </main>
     </div>
